51af484a8f53a41acec64efac457a336d78dd5ff
Shailesh Sony <shailesh.achiever@gmail.com>
2024-08-05 16:09:24 +0530

    Updates

---
+++ precache-manifest.f65f4c876aa315567c4189d653396ba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2645f7ec9bc9e5852a6482f1c104510c",
+    "url": "/redux-crud-app/index.html"
+  },
+  {
+    "revision": "9616c5229070c6c3273c",
+    "url": "/redux-crud-app/static/css/main.8648633d.chunk.css"
+  },
+  {
+    "revision": "12629371205ef9afc0ed",
+    "url": "/redux-crud-app/static/js/2.9d467a3e.chunk.js"
+  },
+  {
+    "revision": "7628d4c2fd53b6c7012cdd37e9e16dac",
+    "url": "/redux-crud-app/static/js/2.9d467a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3594b7c33256606d1a9",
+    "url": "/redux-crud-app/static/js/3.f8222889.chunk.js"
+  },
+  {
+    "revision": "9616c5229070c6c3273c",
+    "url": "/redux-crud-app/static/js/main.69bec353.chunk.js"
+  },
+  {
+    "revision": "0b54f557b3d67c7e37b7",
+    "url": "/redux-crud-app/static/js/runtime-main.9498806c.js"
+  }
+]);
